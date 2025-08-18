d4e91d61399c83b40f8747c2e0a87277c3048fdd
PinkCabagge <ugryumova31.12.2000@gmail.com>
2025-08-18 17:16:34 +0700

    Deployed 1499973 with MkDocs version: 1.6.1

--- assets/extra.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-  if (window.location.hash) {
-    const target = document.querySelector(window.location.hash);
-    if (target) {
-      target.classList.add("highlight");
-      setTimeout(() => target.classList.remove("highlight"), 2000);
-    }
-  }
-});
